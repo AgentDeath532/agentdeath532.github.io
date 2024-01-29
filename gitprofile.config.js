2446256964a506c3823c4f664999ed6e1b3a8ed8
AgentDeath253 <me@agentdeath.net>
2024-01-28 16:44:29 -0800

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -37,7 +37,7 @@ const config = {
     'Node.js (Minimal)',
     'MySQL (Minimal)',
     'MTML (Minimal)',
-    'Git' (Minimal),
+    'Git (Minimal)',
     'Json (Fluent)',
     'Python (Minimal)',
     'YML (Fluent)',
